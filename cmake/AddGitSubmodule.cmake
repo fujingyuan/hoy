2d593798b051c3b521de9339aac6d6f14de752bd
CuriousFu <2269930627@qq.com>
2022-10-18 21:43:36 +0800

    v1.1

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -5,6 +5,5 @@ function(add_git_submodule dir)
     execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
     endif()
-
     add_subdirectory(${dir})
 endfunction(add_git_submodule)
